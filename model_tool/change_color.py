5ab256eaa532541c1be4d1975c05ae32b1845649
kdmcser <zqtndfj@sina,com>
2020-04-16 23:59:09 +0800

    finish implement arg parse

--- model_tool/change_color.py
+++ model_tool/change_color.py
@@ -1,10 +1,11 @@
 import sys
 from argparse import ArgumentParser
-
 from config import Config
 
-if __name__ == "__main__":
-    usage = "对目录里的所有图片文件批量进行颜色转换操作"
+
+def create_arg_parser():
+    usage = "%s -i <输入目录> -o <输出目录> -m <rgb_add|rgb_mix|gray_draw> -r <0~255> -g <0~255> -b <0~255>" \
+            " [-s <源颜色占比> -d <目的颜色占比>]" % sys.argv[0]
     description = '''
 递归遍历输入目录，把检测到的图片文件进行颜色转换后写到输出目录相同的相对路径下。
 目前支持格式为bmp, jpg, png和pcx。如果格式不支持，原样拷贝文件。目前暂打算支持三种颜色转换方式：
@@ -17,16 +18,21 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--input_dir", dest="input_dir", help="输入目录")
     parser.add_argument("-o", "--output_dir", dest="output_dir", help="输出目录")
     parser.add_argument("-m", "--method", dest="method", help='''颜色转换方式：
-    add - 加固定rgb分量到原始颜色中，需要配置-r -g -b
-    mix - 混合固定rgb颜色到原始颜色中, 需要配置-r -g -b [-s -d]
+    rgb_add - 加固定rgb分量到原始颜色中，需要配置-r -g -b
+    rgb_mix - 混合固定rgb颜色到原始颜色中, 需要配置-r -g -b [-s -d]
     gray_draw - 灰度化后用固定颜色按亮度着色, 需要配置-r -g -b''')
     parser.add_argument("-r", "--red", dest="red", help="需要转换的颜色红色分量, 示例: 32")
     parser.add_argument("-g", "--green", dest="green", help="需要转换的颜色绿色分量, 示例： 16")
     parser.add_argument("-b", "--blue", dest="blue", help="需要转换的颜色蓝色分量, 示例: 8")
-    parser.add_argument("-s", "--src_property", dest="src_property", help="mix模式中原始颜色所占比例。"
-                                                     "示例1：0.5，示例2：0.1,0.5,0.1，默认值：0.5")
-    parser.add_argument("-d", "--dest_property", dest="dest_property", help="mix模式中原始颜色所占比例。"
-                                                      "示例1：0.5，示例2：0.9,0.5,0.9，默认值：0.5")
+    parser.add_argument("-s", "--src_property", dest="src_property", help="rgb_mix模式中原始颜色所占比例。"
+                                                                          "示例1：0.5，示例2：0.1,0.5,0.1，默认值：0.5")
+    parser.add_argument("-d", "--dest_property", dest="dest_property", help="rgb_mix模式中混合颜色所占比例。"
+                                                                            "示例1：0.5，示例2：0.9,0.5,0.9，默认值：0.5")
+    return parser
+
+
+if __name__ == "__main__":
+    parser = create_arg_parser()
     args = parser.parse_args()
     config = Config()
     if not config.load_from_args(args):
--- model_tool/config.py
+++ model_tool/config.py
@@ -11,4 +11,67 @@ class Config(object):
         pass
 
     def load_from_args(self, args):
-        return False
+        for necessary_arg in ["input_dir", "output_dir", "method", "red", "green", "blue"]:
+            arg_value = args.__dict__.get(necessary_arg)
+            if not arg_value or not arg_value.strip():
+                print("缺少必要参数：%s" % necessary_arg)
+                return False
+            if not self.set_and_convert_field(necessary_arg, args):
+                return False
+
+        for color_arg in ["red", "green", "blue"]:
+            if not self.is_valid_color(color_arg):
+                return False
+
+        if not self.check_and_get_property_args(arg_value.src_property, arg_value.dest_property):
+            return False
+        return True
+
+    # noinspection PyBroadException
+    def set_and_convert_field(self, name, value):
+        initial_value = self.__dict__.get(name)
+        if not initial_value:
+            print("内部错误：未知参数: [%s]" % initial_value)
+            return False
+        if isinstance(initial_value, int):
+            try:
+                value = int(value)
+            except:
+                print("参数：%s，值：%s 非法，需要整数" % (name, value))
+                return False
+        elif isinstance(initial_value, float):
+            try:
+                value = float(value)
+            except:
+                print("参数：%s，值：%s 非法，需要浮点数" % (name, value))
+                return False
+        elif isinstance(initial_value, str):
+            pass
+        else:
+            print("内部错误：无法识别的参数：%s。类型：%s" % (name, type(initial_value)))
+        self.__dict__[name] = value
+        return True
+
+    def is_valid_color(self, name):
+        color_arg = self.__dict__.get(name)
+        if not 0 <= color_arg <= 255:
+            print("参数：%s需要在0~255之间。实际值：%s" % (name, color_arg))
+            return False
+        return True
+
+    def check_and_get_property_args(self, src_property, dest_property):
+        if src_property is None and dest_property is None:
+            self.src_property = self.dest_property = 0.5
+            print("设置原始颜色比例和混合颜色比例默认值为0.5（如果不是rgb_mix模式将不会用到）")
+        elif src_property is None:
+            if not self.set_and_convert_field("dest_property", dest_property):
+                return False
+            self.src_property = 1.0 - self.dest_property
+            print("设置原始颜色比例默认值为%s（如果不是rgb_mix模式将不会用到）" % self.src_property)
+        elif dest_property is None:
+            if not self.set_and_convert_field("src_property", src_property):
+                return False
+            self.dest_property = 1.0 - self.src_property
+            print("设置混合颜色比例默认值为%s（如果不是rgb_mix模式将不会用到）" % self.dest_property)
+        return True
+
